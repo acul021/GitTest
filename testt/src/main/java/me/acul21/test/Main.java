7196f1778fb59aacba24c0181d6127de1fea0936
BuildTools <unconfigured@null.spigotmc.org>
2017-04-18 09:51:02 +0200

    Final Commit 1

--- testt/src/main/java/me/acul21/test/Main.java
+++ testt/src/main/java/me/acul21/test/Main.java
@@ -1,16 +1,29 @@
 package me.acul21.test;
 
+import java.util.ArrayList;
+import java.util.logging.LoggingMXBean;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener{
+	
+	private ArrayList<Player> players=new ArrayList<Player>();
 
 	@Override
 	public void onEnable() {
 		this.getCommand("jump").setExecutor(this);
+		Bukkit.getPluginManager().registerEvents(this, this);
 		Message.sendMessage(Bukkit.getConsoleSender(), "§aPlugin aktiviert!");
 	}
 	
@@ -30,9 +43,22 @@ public class Main extends JavaPlugin {
 			Message.sendMessage(p, "§cActung!");
 			Message.sendMessage(p, "§aDu machst jetzt einen §6SuperJump§a!");
 			
-			p.setVelocity(p.getLocation().getDirection().multiply(5D));
+			p.setVelocity(p.getLocation().getDirection().multiply(1.5D).setY(1D));
+			
+			//Wenn der Spieler den CMD ausführt wird er in die Liste hinzugefügt damit er kein Fallschaden bekommt
+			Bukkit.broadcastMessage("§aHizugefügt");
+			players.add(p);
 		}
 		return true;
+	}
+	
+	//Wenn der Spieler in der Liste ist bekommt er kein Fallschaden
+	@EventHandler
+	public void fallDamage(EntityDamageEvent e){
+		if(e.getCause()==DamageCause.FALL&&e.getEntity() instanceof Player&&players.contains(e.getEntity())){
+			e.setCancelled(true);
+			players.remove(e.getEntity());
+		}
 	}
 	
 }
